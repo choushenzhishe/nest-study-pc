63c697eff14161bd20be72db18465974ae8dc69f
chou <1070549372@qq.com>
2025-06-07 21:40:25 +0800

    feat: 使用antdesign procomponent适配登陆组件

---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,117 @@
+import {
+  AlipayCircleOutlined,
+  LockOutlined,
+  MobileOutlined,
+  TaobaoCircleOutlined,
+  WeiboCircleOutlined,
+} from '@ant-design/icons';
+import {
+  LoginForm,
+  ProConfigProvider,
+  ProFormCaptcha,
+  ProFormCheckbox,
+  ProFormText,
+  setAlpha,
+} from '@ant-design/pro-components';
+import { Space, Tabs, message, theme } from 'antd';
+import type { CSSProperties } from 'react';
+import { useState } from 'react';
+import './index.module.scss'
+
+type LoginType = 'phone' | 'account';
+
+export default () => {
+  const { token } = theme.useToken();
+  const [loginType, setLoginType] = useState<LoginType>('phone');
+
+  const iconStyles: CSSProperties = {
+    marginInlineStart: '16px',
+    color: setAlpha(token.colorTextBase, 0.2),
+    fontSize: '24px',
+    verticalAlign: 'middle',
+    cursor: 'pointer',
+  };
+
+  return (
+    <ProConfigProvider hashed={false}>
+      <div style={{ backgroundColor: token.colorBgContainer }}>
+        <LoginForm
+          logo="https://chou-nest-study.oss-cn-shenzhen.aliyuncs.com/user-dirhenglogo%402x.png"
+        >
+          <Tabs
+            centered
+            activeKey={loginType}
+            onChange={(activeKey) => setLoginType(activeKey as LoginType)}
+          >
+            <Tabs.TabPane key={'phone'} tab={'手机号登录'} />
+          </Tabs>
+
+          {loginType === 'phone' && (
+            <>
+              <ProFormText
+                fieldProps={{
+                  size: 'large',
+                  prefix: <MobileOutlined className={'prefixIcon'} />,
+                }}
+                name="mobile"
+                placeholder={'手机号'}
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入手机号！',
+                  },
+                  {
+                    pattern: /^1\d{10}$/,
+                    message: '手机号格式错误！',
+                  },
+                ]}
+              />
+              <ProFormCaptcha
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={'prefixIcon'} />,
+                }}
+                captchaProps={{
+                  size: 'large',
+                }}
+                placeholder={'请输入验证码'}
+                captchaTextRender={(timing, count) => {
+                  if (timing) {
+                    return `${count} ${'获取验证码'}`;
+                  }
+                  return '获取验证码';
+                }}
+                name="captcha"
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入验证码！',
+                  },
+                ]}
+                onGetCaptcha={async () => {
+                  message.success('获取验证码成功！验证码为：1234');
+                }}
+              />
+            </>
+          )}
+          <div
+            style={{
+              marginBlockEnd: 24,
+            }}
+          >
+            <ProFormCheckbox noStyle name="autoLogin">
+              自动登录
+            </ProFormCheckbox>
+            <a
+              style={{
+                float: 'right',
+              }}
+            >
+              忘记密码
+            </a>
+          </div>
+        </LoginForm>
+      </div>
+    </ProConfigProvider>
+  );
+};
